f34b73f7db0a154747c75f138b2df6a607fd04a7
eljeilany <sidimed.jeilany@gmail.com>
2016-07-06 12:58:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ var port     = 3000;
 
 var router = express.Router();
 
-
-
-
 // middleware to use for all requests
 router.use(function(req, res, next) {
 	// do logging
@@ -24,29 +21,12 @@ router.use(function(req, res, next) {
 	next();
 });
 
-// router.get('/', function(req, res) {
-// 	res.json({ message: 'call /' });
-// });
-
 // on routes that end in
 // ----------------------------------------------------
 router.route('/airlines')
 	.get(function(req, res) {
 		airplains(req,res)
 	});
-
-// // on routes that end in
-// // ----------------------------------------------------
-// router.route('/airlines')
-//
-// 	.post(function(req, res) {
-// 		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-// 		res.json({ message: Nodes});
-// 	})
-//
-// 	.get(function(req, res) {
-// 		res.json({ message: Nodes });
-// 	});
 
 // on routes that end in
 // ----------------------------------------------------
